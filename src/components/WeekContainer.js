ec311d0bbca51a85776b7f3dc12e92b179933443
Dev Juan <navarroj1215@gmail.com>
2020-06-25 22:55:04 -0500

    Added React Router

--- src/components/WeekContainer.js
+++ src/components/WeekContainer.js
@@ -3,20 +3,6 @@ import DailyCard from './DailyCard'
 
 const WeekContainer = (props) => {
 
-  // componentDidMount = () => {
-  //   const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=60506,us&appid=${apiConfig.omwKey}`
-
-  //   fetch(weatherURL)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-  //       this.setState({
-  //         fullData: data.list,
-  //         dailyData
-  //       }, () => console.log(this.state))
-  //     })
-  // }
-
   const formatDailyCards = () => {
     return props.dailyData.map((reading, index) => <DailyCard reading={reading} key={index} />)
   }
